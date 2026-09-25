55174854d6c045a5e1b4628ecdeed3ef5a947222
Koen Hoefsloot <koenhoefsloot@gmail.com>
2026-09-25 19:23:24 +0200

    Fix dynamic UI encoding

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bartab-shell-v3';
+const CACHE_NAME = 'bartab-shell-v4';
 const APP_SHELL = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
