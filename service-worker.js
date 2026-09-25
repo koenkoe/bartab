b4f57df6540cd45fa56280171480f18d89043c81
Koen Hoefsloot <koenhoefsloot@gmail.com>
2026-09-25 19:34:50 +0200

    Expire admin access and show customer history

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bartab-shell-v5';
+const CACHE_NAME = 'bartab-shell-v6';
 const APP_SHELL = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
