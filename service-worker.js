6912622e58436fea0ce9f28501fe2b39984a11db
Koen Hoefsloot <koenhoefsloot@gmail.com>
2026-09-25 19:43:40 +0200

    Restore home customer tile

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bartab-shell-v6';
+const CACHE_NAME = 'bartab-shell-v7';
 const APP_SHELL = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
