cce5d5604a557bf27a872ce5f8b844b123c89a44
Koen Hoefsloot <koenhoefsloot@gmail.com>
2026-09-25 19:28:54 +0200

    Hide home customer from homescreen

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bartab-shell-v4';
+const CACHE_NAME = 'bartab-shell-v5';
 const APP_SHELL = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
